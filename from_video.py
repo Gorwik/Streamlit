888ffa21cc0d1f350857ac0000743484b5304300
Wiktor Góra <xw.gora.x@gmail.com>
2022-09-27 16:15:25 +0200

    first streamlit

---
+++ from_video.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Our first streamlit App")
+st.subheader("Introducing Streamlit in Automate Everything with Python")
+st.write(
+    """This is our first App.
+Enjoy it
+"""
+)
